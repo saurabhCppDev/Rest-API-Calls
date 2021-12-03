dc3fc72295bc9598780c8717fb9ec337bc9c786b
sonali2495 <sonaligadge24@gmail.com>
2021-12-03 19:50:27 +0530

    Added: POST Request Method and Pass First Name and Last Name in The Body

--- src/main/java/com/bridgelabz/restapicalls/controller/HelloWorldController.java
+++ src/main/java/com/bridgelabz/restapicalls/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.restapicalls.controller;
 
+import com.bridgelabz.restapicalls.model.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,20 +9,28 @@ public class HelloWorldController {
 
     //curl localhost:8080/hello -w "\n"
     @RequestMapping(value = {"", "/", "/home"})
-    public String sayHello(){
+    public String sayHello() {
         return "Hello from Bridgelabz!";
     }
 
     //curl localhost:8080/hello/query?name=Sonali -w "\n"
     @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
-    public String sayHello(@RequestParam(value = "name") String name){
+    public String sayHello(@RequestParam(value = "name") String name) {
         return "Hello " + name + "!";
     }
 
     //curl localhost:8080/hello/param/Sonali -w "\n"
     @GetMapping("/param/{name}")
-    public String sayHelloParam(@PathVariable String name){
+    public String sayHelloParam(@PathVariable String name) {
         return "Hello " + name + "!";
+    }
+
+    //curl -X POST -H "Content-Type: application/json"
+    //      -d '{"firstName: "Lisa", "lastName": "Harry"}'
+    //      "http://localhost:8080/hello/post" -w "\n"
+    @PostMapping("/post")
+    public String sayHello(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + "!";
     }
 
 }
---
+++ src/main/java/com/bridgelabz/restapicalls/model/User.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.restapicalls.model;
+
+public class User {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
